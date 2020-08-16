b06cc538da1e38a75b8e5d621dd05739bacd39d1
史恭文 <844910815@qq.com>
2020-08-16 15:40:05 +0800

    测试

---
+++ src/main/java/com/sleep/owl/forjenkis/ForJenkisApplication.java
@@ -0,0 +1,13 @@
+package com.sleep.owl.forjenkis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForJenkisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForJenkisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sleep/owl/forjenkis/ForJenkisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleep.owl.forjenkis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForJenkisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
